770df309636ee5f89620e9560dc058ae412e3a69
Vadym <vadym@vadyms-macbook-air.home>
2023-03-03 23:10:58 +0100

    Ops/Added CD. Added precommit hook

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 import { SortOrder } from '@elastic/elasticsearch/lib/api/types';
 
 export const RABBITMQ_URI = process.env.RABBITMQ_URI || 'amqp://localhost:5672';
-export const ROWS_PER_SEARCH = process.env.ROWS_PER_SEARCH || '5';
+export const ELASTICSEARCH_URI =
+  process.env.ELASTICSEARCH_URI || 'http://localhost:9200';
 export const DEFAULT_SORT: SortOrder = 'desc';
+export const PAGING_ITEMS_PER_PAGE = process.env.PAGING_ITEMS_PER_PAGE || '10';
